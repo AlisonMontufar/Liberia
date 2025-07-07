04a22dea57110e02a70410bb3f55e50da0fc01fd
Alison Montufar <alisonmontufar14@gmail.com>
2025-07-07 17:09:01 -0600

    Cambio de url en produccion

--- src/pages/LibrosPage.js
+++ src/pages/LibrosPage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const API_LIBROS = 'https://libromicroservicio.somee.com';
-const API_AUTORES = 'https://localhost:32785/api/Autor';
+const API_AUTORES = 'https://localhost:32781/api/Autor';
 
 const LibrosPage = () => {
   const [libros, setLibros] = useState([]);
